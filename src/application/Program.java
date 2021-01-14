a9e2e34af114f044bdb5f0bc9f657081edd50c91
José L. F. Silva <jose.silva1463@etec.sp.gov.br>
2021-01-14 13:35:52 -0300

    FindById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,16 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department d = new Department(1, "Books");
-		System.out.println(d);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
-		Seller s = new Seller(1, "José", "jose.silva1463@etec.sp.gov.br", new Date(), 3000.0, d);
-		System.out.println(s);
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
 	}
 
 }
